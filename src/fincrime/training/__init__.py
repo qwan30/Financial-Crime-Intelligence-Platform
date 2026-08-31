9fc2786d74fcd9b005478c9f8a829647264b3c30
qwan30 <tranthanhquan09@gmail.com>
2026-08-31 11:53:57 +0700

    feat(training): gate advanced GNN research

--- src/fincrime/training/__init__.py
+++ src/fincrime/training/__init__.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
 
+from fincrime.training.advanced_gate import (
+    AdvancedModelDecision,
+    advanced_model_decision,
+)
 from fincrime.training.baselines import (
     ProbabilityModel,
     fit_lightgbm,
@@ -10,12 +14,22 @@ from fincrime.training.gates import (
     PromotionDecision,
     promotion_decision,
 )
+from fincrime.training.graphsage import (
+    GraphSAGEDetector,
+    build_neighbor_loader,
+    train_graphsage_epoch,
+)
 
 __all__ = [
+    "AdvancedModelDecision",
+    "GraphSAGEDetector",
     "ProbabilityModel",
     "PromotionDecision",
+    "advanced_model_decision",
+    "build_neighbor_loader",
     "fit_lightgbm",
     "fit_logistic",
     "predict_scores",
     "promotion_decision",
+    "train_graphsage_epoch",
 ]
